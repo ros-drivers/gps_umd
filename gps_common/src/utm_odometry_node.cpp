d5b1b305089b8eca818e59c92a0de495689f1eb9
Ken Tossell <ken@tossell.net>
2011-01-30 19:22:58 -0500

    Updating UTM GPS->Odometry converter to use NavSatFix. Adding covariance.

--- gps_common/src/utm_odometry_node.cpp
+++ gps_common/src/utm_odometry_node.cpp
@@ -1,21 +1,23 @@
 /*
- * Translates gps_common/GPS{Fix,Status} into nav_msgs/Odometry using UTM
+ * Translates sensor_msgs/NavSat{Fix,Status} into nav_msgs/Odometry using UTM
  */
 
 #include <ros/ros.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
-#include <gps_common/GPSStatus.h>
-#include <gps_common/GPSFix.h>
+#include <sensor_msgs/NavSatStatus.h>
+#include <sensor_msgs/NavSatFix.h>
 #include <gps_common/conversions.h>
 #include <nav_msgs/Odometry.h>
 
 using namespace gps_common;
 
 static ros::Publisher odom_pub;
+std::string frame_id;
+double rot_cov;
 
-void callback(const GPSStatusConstPtr& status, const GPSFixConstPtr& fix) {
-  if (status->status == GPSStatus::STATUS_NO_FIX) {
+void callback(const sensor_msgs::NavSatFixConstPtr& fix) {
+  if (fix->status.status == sensor_msgs::NavSatStatus::STATUS_NO_FIX) {
     ROS_INFO("No fix.");
     return;
   }
@@ -28,9 +30,41 @@ void callback(const GPSStatusConstPtr& status, const GPSFixConstPtr& fix) {
   if (odom_pub) {
     nav_msgs::Odometry odom;
     odom.header.stamp = fix->header.stamp;
+
+    if (frame_id.empty())
+      odom.header.frame_id = fix->header.frame_id;
+    else
+      odom.header.frame_id = frame_id;
+
     odom.pose.pose.position.x = easting;
     odom.pose.pose.position.y = northing;
-    odom.pose.pose.position.z = 0.0;
+    odom.pose.pose.position.z = fix->altitude;
+    
+    odom.pose.pose.orientation.x = 1;
+    odom.pose.pose.orientation.y = 0;
+    odom.pose.pose.orientation.z = 0;
+    odom.pose.pose.orientation.w = 0;
+    
+    // Use ENU covariance to build XYZRPY covariance
+    boost::array<double, 36> covariance = {{
+      fix->position_covariance[0],
+      fix->position_covariance[1],
+      fix->position_covariance[2],
+      0, 0, 0,
+      fix->position_covariance[3],
+      fix->position_covariance[4],
+      fix->position_covariance[5],
+      0, 0, 0,
+      fix->position_covariance[6],
+      fix->position_covariance[7],
+      fix->position_covariance[8],
+      0, 0, 0,
+      0, 0, 0, rot_cov, 0, 0,
+      0, 0, 0, 0, rot_cov, 0,
+      0, 0, 0, 0, 0, rot_cov
+    }};
+
+    odom.pose.covariance = covariance;
 
     odom_pub.publish(odom);
   }
@@ -39,16 +73,15 @@ void callback(const GPSStatusConstPtr& status, const GPSFixConstPtr& fix) {
 int main (int argc, char **argv) {
   ros::init(argc, argv, "utm_odometry_node");
   ros::NodeHandle node;
+  ros::NodeHandle priv_node(node, "~");
 
-  message_filters::Subscriber<GPSStatus> status_sub(node, "status", 1);
-  message_filters::Subscriber<GPSFix> fix_sub(node, "fix", 1);
-  message_filters::TimeSynchronizer<GPSStatus, GPSFix> sync(status_sub, fix_sub, 10);
+  priv_node.param<std::string>("frame_id", frame_id, "");
+  priv_node.param<double>("rot_covariance", rot_cov, 99999.0);
 
   odom_pub = node.advertise<nav_msgs::Odometry>("odom", 10);
 
-  sync.registerCallback(
-    boost::bind(&callback, _1, _2)
-  );
+  ros::Subscriber fix_sub = node.subscribe("fix", 10, callback);
 
   ros::spin();
 }
+
